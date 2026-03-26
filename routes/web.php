f9d3c31b4f532f261c7496d1d2cd3fa851b0ea0f
Eduardo Ferraz <dudiferraz@gmail.com>
2026-03-26 14:28:08 -0300

    chore: remove unnecessary views and endpoints generated by starter

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Permite usar Gate::check('user')na view 404
 Route::fallback(function(){
     return view('errors.404');
