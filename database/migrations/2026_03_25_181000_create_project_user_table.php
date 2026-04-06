6a29b02be6a604d904701e22f50308265f28ce04
Eduardo Ferraz <dudiferraz@gmail.com>
2026-04-06 15:31:32 -0300

    fix: adding require_once to match ProjectUserRole enum new location

--- database/migrations/2026_03_25_181000_create_project_user_table.php
+++ database/migrations/2026_03_25_181000_create_project_user_table.php
@@ -5,6 +5,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema; 
 
+require_once app_path('Models/ProjectUser.php');
+
 return new class extends Migration
 {
     public function up(): void
