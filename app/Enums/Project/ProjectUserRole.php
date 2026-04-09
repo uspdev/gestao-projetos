1b81e63b6a8ea13cedc045020c4a8a9b028b8c72
Eduardo Ferraz <dudiferraz@gmail.com>
2026-04-09 17:38:01 -0300

    feat(ProjectUserRole): implementing color and label methods

--- app/Enums/Project/ProjectUserRole.php
+++ app/Enums/Project/ProjectUserRole.php
@@ -7,4 +7,22 @@ enum ProjectUserRole: string
     case OWNER = 'OWNER';
     case MEMBER = 'MEMBER';
     case VIEWER = 'VIEWER';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::OWNER => 'Responsavel',
+            self::MEMBER => 'Membro',
+            self::VIEWER => 'Visualizador',
+        };
+    }
+
+    public function color(): string
+    {
+        return match ($this) {
+            self::OWNER => 'badge-primary',
+            self::MEMBER => 'badge-info',
+            self::VIEWER => 'badge-secondary',
+        };
+    }
 }
