b75a041492323ceac46db9ec71c891392d56c744
Eduardo Ferraz <dudiferraz@gmail.com>
2026-04-14 15:07:13 -0300

    feat(project) adding start_date to show project endpoint

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -45,7 +45,7 @@ class ProjectController extends Controller
         Gate::authorize('view', $project);
         $project = $project->load([
             'users:id,name,email',
-            'tasks:id,project_id,title,priority,status,label,due_date',
+            'tasks:id,project_id,title,priority,status,label,start_date,due_date',
         ]);
 
         return view('projects.show', compact('project'));
