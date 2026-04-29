cc4abdc9f1dc900ca3e54207576edb227a42796e
Masaki Kawabata Neto <kawabata@usp.br>
2026-04-29 09:17:39 -0300

    revert: remove ide helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,13 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Event::listen(MigrationsEnded::class, function () {
-            if (app()->environment('local')) {
-                Artisan::call('ide-helper:models', ['--write' => true]);
-                echo "\n \033[32mINFO\033[0m Tipagem dos Models atualizada com sucesso pelo IDE Helper.\n";
-            }
-        });
-
         View::composer('laravel-usp-theme::master', function () {
             $routeName = request()->route()?->getName();
 
