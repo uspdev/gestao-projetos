2e81eba71c563a0542aaafd9074db12ba5302ca7
Masaki Kawabata Neto <kawabata@usp.br>
2026-04-30 11:14:33 -0300

    feat: mostra responsáveis

--- resources/views/projects/partials/show-card-tasks-table.blade.php
+++ resources/views/projects/partials/show-card-tasks-table.blade.php
@@ -1,7 +1,7 @@
 {{-- Index: Lista de Tasks --}}
 <div class="card mb-4 shadow-sm">
   <div class="card-header h5">
-     <i class="fas fa-tasks"></i> Tarefas
+    <i class="fas fa-tasks"></i> Tarefas
     @include('tasks.partials.create-task-btn')
     @include('tasks.partials.show-done-btn')
   </div>
@@ -14,6 +14,7 @@
             <th title="Prioridade">Prio.</th>
             <th>Status</th>
             <th>Título</th>
+            <th>Responsável</th>
             <th>Tags</th>
             <th>Criado em</th>
           </tr>
@@ -25,26 +26,22 @@
                 @include('tasks.partials.edit')
               </td>
               <td>
-                <span class="badge {{ $task->priority?->color() }}">
-                  {{ $task->priority?->label() }}
-                </span>
+                <span class="badge {{ $task->priority?->color() }}">{{ $task->priority?->label() }}</span>
               </td>
               <td>
-                <span class="badge {{ $task->status->color() }}">
-                  {{ $task->status->label() }}
-                </span>
+                <span class="badge {{ $task->status->color() }}">{{ $task->status->label() }}</span>
               </td>
-
               <td>
                 <a href="{{ route('tasks.show', $task) }}" class="text-decoration-none">
                   {{ $task->title }}
                 </a>
               </td>
               <td>
+                {{ $task->users->pluck('name')->implode(', ') }}
+              </td>
+              <td>
                 @foreach ($task->tags as $tag)
-                  <span class="badge badge-secondary">
-                    {{ $tag->name }}
-                  </span>
+                  <span class="badge badge-secondary">{{ $tag->name }}</span>
                 @endforeach
               </td>
               <td data-order="{{ $task->created_at?->format('Y-m-d H:i:s') }}">
