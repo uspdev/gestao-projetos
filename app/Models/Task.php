7f95f87e9f0ec9a6e4345777e36a6567aaea71b1
Masaki Kawabata Neto <kawabata@usp.br>
2026-04-30 11:14:22 -0300

    refact: clean

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -13,56 +13,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Tags\HasTags;
 
-/**
- * @property int $id
- * @property int $project_id
- * @property string $title
- * @property string|null $description
- * @property string|null $priority
- * @property string $status
- * @property \Illuminate\Support\Carbon|null $start_date
- * @property \Illuminate\Support\Carbon|null $due_date
- * @property int|null $created_by
- * @property int|null $updated_by
- * @property int|null $deleted_by
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property-read \App\Models\Project|null $project
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
- * @property-read int|null $users_count
- * @method static \Database\Factories\TaskFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereDueDate($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereLabel($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task wherePriority($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereProjectId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereStartDate($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withTrashed(bool $withTrashed = true)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withoutTrashed()
- * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tag> $tags
- * @property-read int|null $tags_count
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withAllTags(\ArrayAccess|\Spatie\Tags\Tag|array|string $tags, ?string $type = null)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withAllTagsOfAnyType($tags)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withAnyTags(\ArrayAccess|\Spatie\Tags\Tag|array|string $tags, ?string $type = null)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withAnyTagsOfAnyType($tags)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withAnyTagsOfType(array|string $type)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Task withoutTags(\ArrayAccess|\Spatie\Tags\Tag|array|string $tags, ?string $type = null)
- * @mixin \Eloquent
- */
 class Task extends Model
 {
     use HasFactory, SoftDeletes, Auditable, HasTags;
