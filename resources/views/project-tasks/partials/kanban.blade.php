639f9fcf9a5a109b62bc219eeeb0c2e375b8796e
Eduardo Ferraz <dudiferraz@gmail.com>
2026-05-06 16:30:34 -0300

    fix(tasks): renomeando include de partials errados que impedia a renderização do botão 'ver em kanban'

--- resources/views/project-tasks/partials/kanban.blade.php
+++ resources/views/project-tasks/partials/kanban.blade.php
@@ -2,11 +2,11 @@
   <div class="card-header h5">
     <i class="fas fa-tasks"></i> Tarefas
     @include('tasks.partials.create-task-btn')
-    @includeIf('user-tasks.partials.toggle-layout-btn', ['view' => $view])
+    @include('tasks.partials.toggle-layout-btn', ['view' => $view])
     @include('tasks.partials.show-done-btn')
   </div>
 
   <div class="card-body">
-    @include('user-tasks.partials.kanban', ['showDone' => $showDone])
+    @include('tasks.partials.kanban.kanban', ['showDone' => $showDone])
   </div>
 </div>
