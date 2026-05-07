26ab3424509e85beab3b436abf7b943c30f20515
Masaki Kawabata Neto <kawabata@usp.br>
2026-05-07 12:25:57 -0300

    fix: cleanup

--- resources/views/projects/partials/show-header.blade.php
+++ resources/views/projects/partials/show-header.blade.php
@@ -1,8 +1,5 @@
 @php
-  // $class(['btn', 'btn-sm', 'btn-outline-secondary'=> $routeName === 'projects.tasks.index'])>
-
   $routeName = Route::currentRouteName();
-  echo $routeName;
 @endphp
 
 <div class="card-header d-flex justify-content-between align-items-center gap-2 card-header-sticky">
@@ -11,7 +8,7 @@
       <i class="fas fa-home"></i>
     </a>
     <x-separator />
-    <span class="btn btn-sm btn-primary"> {{ $project->status?->label() }}</span>
+    <span class="badge badge-pill badge-primary" style="font-size: 0.75rem; padding: 0.2rem 0.4rem;"> {{ $project->status?->label() }}</span>
     {{ $project->name }}
 
     <a href="{{ route('projects.show', $project) }}"
@@ -28,13 +25,10 @@
   <div class="d-flex align-items-center gap-2">
     @include('projects.partials.show-tag-badges')
 
-
     <a href="{{ route('projects.settings', $project) }}"
       class="btn btn-sm
      {{ $routeName === 'projects.settings' ? 'btn-warning' : 'btn-outline-warning' }}">
       <i class="fas fa-cog"></i> Configurações
     </a>
-
   </div>
-
 </div>
