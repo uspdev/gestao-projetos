342820f9b318226aa4fa030f15fa7640ddbe3801
Eduardo Ferraz <dudiferraz@gmail.com>
2026-05-12 16:15:40 -0300

    feat(project type): create ProjectType Model

---
+++ app/Models/ProjectType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasSlug;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class ProjectType extends Model
+{
+    use HasFactory, HasSlug;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+    /**
+     * Relacionamento com projetos 1-N
+     */
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    /**
+     * Relacionamento com configurações de módulos 1-N
+     */
+    public function projectTypeModules(): HasMany
+    {
+        return $this->hasMany(ProjectTypeModule::class);
+    }
+
+    /**
+     * Relacionamento com módulos N-N
+     */
+    public function modules(): BelongsToMany
+    {
+        return $this->belongsToMany(Module::class, 'project_type_modules')
+            ->withPivot('enabled', 'required', 'editable', 'config')
+            ->withTimestamps();
+    }
+
+    public function setSlugAttribute($value): void
+    {
+        $this->attributes['slug'] = $value === null ? null : Str::slug((string) $value);
+    }
+}
