288a222ed56631a583e38f42931993046cfe2e39
Masaki Kawabata Neto <kawabata@usp.br>
2026-05-19 12:29:12 -0300

    feat: incuido autofocus

--- resources/views/projects/partials/components/search-project-form.blade.php
+++ resources/views/projects/partials/components/search-project-form.blade.php
@@ -1,6 +1,6 @@
 <form method="GET" action="{{ route('projects.index') }}" class="d-flex align-items-center ml-4 mt-2" style="gap: .5rem;">
   <input id="project-search" name="search" type="search" class="form-control form-control-sm" placeholder="Buscar projeto"
-    value="{{ $search }}" style="width: 250px;">
+    value="{{ $search }}" style="width: 250px;" autofocus>
   <button type="submit" class="btn btn-primary btn-sm">Buscar</button>
   @if ($search !== '')
     <a href="{{ route('projects.index') }}" class="btn btn-outline-secondary btn-sm">Limpar</a>
