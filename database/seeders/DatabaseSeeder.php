5cfcf63c0285cfa55357605f38b3a05150a1d1f9
Lucas Lopes <lucaslops12@gmail.com>
2026-05-26 17:35:33 -0300

    refact(seeders): remove seeders e factories não ultilizadas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,34 +2,12 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\ProjectTypeSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        if (app()->environment(['local', 'testing'])) {
-            $this->call([
-                ModuleSeeder::class,
-            ]);
-        }
 
-        $this->call([
-            UserSeeder::class,
-            ProjectSeeder::class,
-        ]);
-
-        if (app()->environment(['local', 'testing'])) {
-            $this->call([
-                ProjectTypeSeeder::class,
-                ProjectTypeModuleSeeder::class,
-            ]);
-        }
-
-        $this->call([
-            ProjectModuleSeeder::class,
-            TaskSeeder::class,
-        ]);
     }
 }
