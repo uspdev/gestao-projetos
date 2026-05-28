ba5f583b28e552ee1ee596c54af181fab1444cf5
Lucas Lopes <lucaslops12@gmail.com>
2026-05-28 14:25:40 -0300

    feat(ui): incrementar página de subprojetos para ficar similar a visão geral. Closes #13

--- resources/views/projects/subprojects.blade.php
+++ resources/views/projects/subprojects.blade.php
@@ -8,8 +8,14 @@
       @include('projects.partials.show.show-card-subprojects')
     </div>
     <div class="col-md-4">
+      <div class="mb-4">
+        @include('projects.partials.show.project-type-card')
+      </div>
+      @include('projects.partials.show.show-card-modulos', [
+          'resolvedModules' => $project->activeModulesSummary(),
+      ])
       @include('projects.partials.show.show-card-descricao')
-      {{-- @include('projects.partials.show.show-card-meetings') --}}
     </div>
+  </div>
 
-  @endsection
+@endsection
