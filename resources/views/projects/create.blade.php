4c3454bcdd33d8ee90f155f679611b3652c77f0c
Masaki Kawabata Neto <kawabata@usp.br>
2026-06-01 11:45:22 -0300

    refact: view de novo projeto reestruturado

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -10,30 +10,44 @@
     </div>
 
     <div class="row">
+      <div class="col-md-4 mb-3">
+        <div class="card h-100">
+          <div class="card-body d-flex flex-column">
+            <div class="d-flex justify-content-between align-items-start mb-3">
+              <h5 class="card-title mb-0">{{ $organizacional->name }}</h5>
+            </div>
+            <div class="text-muted"><x-markdown-content :text="$organizacional->description" :escape-html="false" /></div>
+            <div class="mb-3">
+              <strong class="d-block mb-2">Módulos ativos</strong>
+              <ul class="mb-0">
+                @foreach ($organizacional->enabledModules() as $module)
+                  <li>{{ $module->name }}</li>
+                @endforeach
+              </ul>
+            </div>
+            <a class="btn btn-success mt-auto"
+              href="{{ route('projects.create', ['project_type' => $organizacional->slug]) }}">
+              Criar
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
       @forelse ($projectTypes as $projectType)
-        @php
-          $activeModules = $projectType->modules
-              ->filter(fn($module) => (bool) ($module->pivot?->enabled ?? false))
-              ->values();
-        @endphp
         <div class="col-md-4 mb-3">
           <div class="card h-100">
             <div class="card-body d-flex flex-column">
               <div class="d-flex justify-content-between align-items-start mb-3">
                 <h5 class="card-title mb-0">{{ $projectType->name }}</h5>
               </div>
-
-              @if ($projectType->description)
-                <div class="text-muted"><x-markdown-content :text="$projectType->description" :escape-html="false" /></div>
-              @else
-                <p class="text-muted">Sem descrição cadastrada para este tipo de projeto.</p>
-              @endif
-
+              <div class="text-muted"><x-markdown-content :text="$projectType->description" :escape-html="false" /></div>
               <div class="mb-3">
                 <strong class="d-block mb-2">Módulos ativos</strong>
-                @if ($activeModules->isNotEmpty())
+                @if ($projectType->enabledModules()->isNotEmpty())
                   <ul class="mb-0">
-                    @foreach ($activeModules as $module)
+                    @foreach ($projectType->enabledModules() as $module)
                       <li>{{ $module->name }}</li>
                     @endforeach
                   </ul>
@@ -57,5 +71,6 @@
         </div>
       @endforelse
     </div>
+
   </div>
 @endsection
