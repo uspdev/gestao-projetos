86562ccf6ee57e55c1216fe1e6dbec729bcde800
Masaki Kawabata Neto <kawabata@usp.br>
2026-06-01 11:49:44 -0300

    fix: pinnedProjects: ordenando por nome

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
      */
     public function pinnedProjects()
     {
-        return $this->projects()->wherePivot('pinned', true)->get();
+        return $this->projects()->wherePivot('pinned', true)->orderBy('name')->get();
     }
 
     /**
