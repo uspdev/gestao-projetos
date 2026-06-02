f32861f84c6e4ec2c083587cb2545709a9ba0e91
Masaki Kawabata Neto <kawabata@usp.br>
2026-06-02 08:53:48 -0300

    feat: mudando ordem de card

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -12,9 +12,9 @@
     </div>
     <div class="col-md-4">
       <x-projects::show.tipo-card :project="$project" />
-      @include('projects.partials.show.show-card-modulos')
       <x-projects::show.subprojects-card :project="$project" />
       <x-projects::show.membros-preview-card :project="$project" />
+      @include('projects.partials.show.show-card-modulos')
     </div>
   </div>
 @endsection
