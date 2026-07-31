264a19cbd49ba632107a1f409d4197f80825c445
Lucas Lopes <lucaslops12@gmail.com>
2026-07-31 14:54:00 -0300

    fix(markdown): adicionar rolagem aos textos longos

--- resources/views/components/markdown-content.blade.php
+++ resources/views/components/markdown-content.blade.php
@@ -1,4 +1,9 @@
-@props(['text' => ''])
+@props(['text' => '', 'ariaLabel' => 'Conteúdo em Markdown'])
 @inject('markdownRenderer', 'App\Services\MarkdownRenderer')
 
-<div class="markdown-content">{!! $markdownRenderer->render((string) $text) !!}</div>
+<div {{ $attributes->class(['markdown-content', 'overflow-auto', 'pr-2'])->merge([
+    'aria-label' => $ariaLabel,
+    'role' => 'region',
+    'style' => 'max-height: min(60vh, 36rem); overscroll-behavior-y: contain;',
+    'tabindex' => '0',
+]) }}>{!! $markdownRenderer->render((string) $text) !!}</div>
